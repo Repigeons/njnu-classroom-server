fc42c0cf81c38c61845e5e058bc5ae2d01d64ed5
Zhou Tianxing <zhoutianxing@repigeons.cn>
2023-09-08 23:41:22 +0800

    fix: gateway blocking url

--- njnu-classroom-gateway/src/main/kotlin/cn/repigeons/njnu/classroom/config/BlockingUrlConfig.kt
+++ njnu-classroom-gateway/src/main/kotlin/cn/repigeons/njnu/classroom/config/BlockingUrlConfig.kt
@@ -1,17 +1,24 @@
 package cn.repigeons.njnu.classroom.config
 
+import com.alibaba.cloud.nacos.NacosConfigProperties
+import com.alibaba.nacos.api.NacosFactory
+import com.alibaba.nacos.api.config.listener.Listener
+import jakarta.annotation.PostConstruct
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import org.springframework.web.util.pattern.PathPatternRouteMatcher
+import org.yaml.snakeyaml.Yaml
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+
 
 @Configuration
 class BlockingUrlConfig(
     private val blockingUrlProperties: BlockingUrlProperties,
+    private val nacosConfigProperties: NacosConfigProperties,
 ) : WebFilter {
     private val matcher = PathPatternRouteMatcher()
     private val forbiddenResponse = "{\"status\":403,\"message\":\"Blocking URL\",\"data\":null}".toByteArray()
@@ -35,5 +42,21 @@ class BlockingUrlConfig(
             }
         }
         return chain.filter(exchange)
+    }
+
+    @PostConstruct
+    fun autoRefresh() {
+        val configService = NacosFactory.createConfigService(nacosConfigProperties.assembleConfigServiceProperties())
+        configService.addListener(nacosConfigProperties.name, nacosConfigProperties.group, object : Listener {
+            private val yaml = Yaml()
+            override fun getExecutor() = null
+            override fun receiveConfigInfo(configInfo: String) {
+                val map = yaml.load<Map<String, *>>(configInfo)
+                val blocking = map["blocking"]?.let { yaml.dump(it) }
+                val properties = yaml.load<BlockingUrlProperties>(blocking)
+                blockingUrlProperties.path = properties.path
+                blockingUrlProperties.pattern = properties.pattern
+            }
+        })
     }
 }

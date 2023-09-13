c463fe8a13c6d339caa7d2a10dd3c48f9dbe6ee5
Zhou Tianxing <zhoutianxing@repigeons.cn>
2023-09-13 16:33:55 +0800

    fix: FeignRequestHeaderInterceptor

--- njnu-classroom-commons/src/main/kotlin/cn/repigeons/njnu/classroom/commons/rpc/FeignRequestHeaderInterceptor.kt
+++ njnu-classroom-commons/src/main/kotlin/cn/repigeons/njnu/classroom/commons/rpc/FeignRequestHeaderInterceptor.kt
@@ -1,26 +1,19 @@
 package cn.repigeons.njnu.classroom.commons.rpc
 
+import cn.dev33.satoken.context.SaHolder
 import feign.RequestInterceptor
 import feign.RequestTemplate
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.server.ServerWebExchange
-import reactor.core.publisher.Mono
-
+import org.springframework.http.server.reactive.ServerHttpRequest
 
 @Configuration
 class FeignRequestHeaderInterceptor : RequestInterceptor {
     override fun apply(template: RequestTemplate) {
-        val exchange = Mono.deferContextual { context ->
-            val exchange = context.get(ServerWebExchange::class.java)
-            Mono.just(exchange)
-        }
-        val headers = exchange.map { it.request.headers }
-        headers.map {
-            it.forEach { key, values ->
-                if (!key.startsWith("content", ignoreCase = true)) {
-                    template.header(key, values)
-                }
+        val request = SaHolder.getRequest().source as ServerHttpRequest
+        request.headers.forEach { key, values ->
+            if (!key.startsWith("content", ignoreCase = true)) {
+                template.header(key, values)
             }
-        }.subscribe()
+        }
     }
 }

7c0afeb8f70c2db59820d891bd6bd8562ef5354f
Zhou Tianxing <zhoutianxing@repigeons.cn>
2023-12-04 14:47:04 +0800

    feat: 优化登录

--- njnu-classroom-spider/src/main/kotlin/cn/repigeons/njnu/classroom/service/impl/CookieServiceImpl.kt
+++ njnu-classroom-spider/src/main/kotlin/cn/repigeons/njnu/classroom/service/impl/CookieServiceImpl.kt
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import java.net.ProxySelector
+import java.net.URL
 
 @Service
 class CookieServiceImpl(
@@ -39,16 +40,21 @@ class CookieServiceImpl(
 
     @Cacheable("cookies")
     override fun getCookies(): List<Cookie> {
+        driver.get("https://authserver.nnu.edu.cn/authserver/login?service=http%3A%2F%2Fehallapp.nnu.edu.cn%2Fjwapp%2Fsys%2Fjsjy%2F*default%2Findex.do%3Famp_sec_version_%3D1%26gid_%3D${gid}")
+        Thread.sleep(5000)
+        if (URL(driver.currentUrl).host == "authserver.nnu.edu.cn") {
+            logger.info("Login with user {}", username)
+            driver.switchTo().defaultContent()
+            try {
+                driver.findElement(By.id("username")).sendKeys(username)
+                driver.findElement(By.id("password")).sendKeys(password)
+                driver.findElement(By.id("login_submit")).click()
+            } catch (_: Exception) {
+            }
+            Thread.sleep(5000)
+        }
         driver.get("http://ehallapp.nnu.edu.cn/jwapp/sys/jsjy/*default/index.do?amp_sec_version_=1&gid_=$gid")
         Thread.sleep(5000)
-        logger.info("Login with user {}", username)
-        driver.switchTo().defaultContent()
-        try {
-            driver.findElement(By.id("username")).sendKeys(username)
-            driver.findElement(By.id("password")).sendKeys(password)
-            driver.findElement(By.id("login_submit")).click()
-        } catch (_: Exception) {
-        }
         return driver.manage().cookies
             .filter { it.name in listOf("MOD_AUTH_CAS", "_WEU") }
             .map {

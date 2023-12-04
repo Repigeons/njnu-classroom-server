482799a487efdeb7bbd16019db7c42d288d0c388
Zhou Tianxing <z.t.x@foxmail.com>
2023-12-04 22:08:14 +0800

    fix: cycle dependencies

--- njnu-classroom-spider/src/main/kotlin/cn/repigeons/njnu/classroom/config/ProxyConfig.kt
+++ njnu-classroom-spider/src/main/kotlin/cn/repigeons/njnu/classroom/config/ProxyConfig.kt
@@ -8,11 +8,13 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
 import java.io.IOException
 import java.net.*
 
 @Configuration
 class ProxyConfig(
+    @Lazy
     private val cookieService: CookieService,
     @Value("\${proxy-pool.all:}")
     private val allProxy: String,
